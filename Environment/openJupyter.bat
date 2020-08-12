aaac4a9f77be700f4d228324ff94a9c3853b0426
Stridd <ro.bert_srg@yahoo.co.uk>
2020-08-12 14:16:02 +0300

    Removed openwithjupyter

--- Environment/openJupyter.bat
+++
@@ -1,7 +0,0 @@
-SET anaconda_path=E:/Programs/Anaconda3/
-
-call %anaconda_path%/Scripts/activate.bat %anaconda_path%
-echo Activating Cython Environment
-call conda activate torchRL
-
-call jupyter-notebook Tests.ipynb
