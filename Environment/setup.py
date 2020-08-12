c2e9b72e8e16e4c8078215b5c9b4cdc7873af9dd
Stridd <ro.bert_srg@yahoo.co.uk>
2020-08-12 14:15:13 +0300

    Modified gitignore and setup to set cython to python3

--- Environment/setup.py
+++ Environment/setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("Environment_2048.pyx"))
+setup(ext_modules=cythonize("Environment_2048.pyx", compiler_directives={'language_level' : "3"}))
