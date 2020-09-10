Added normalization of returns + some fixes

Environment/Reinforce_Agent.py
@@ -8,7 +8,7 @@ from Parameters import Parameters
 from Plotter import Plotter
 from Data_Helper import Data_Helper
 
-import torch.optim as optim
+
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
@@ -32,8 +32,7 @@ class Reinforce_Agent():
 
         self.gamma  = Parameters.gamma
 
-        self.optimizer = optim.Adam(self.policy.parameters(), lr=Parameters.lr)
-
+        self.optimizer = Utility.get_optimizer_for_parameters(self.policy.parameters())
         self.data_helper = Data_Helper() 
 
     def setup_logger(self, time_of_experiment):
@@ -57,12 +56,14 @@ class Reinforce_Agent():
             future_returns = self.policy.rewards[t] + self.gamma * future_returns
             returns[t] = future_returns
 
-        returns = torch.tensor(returns).to(Parameters.device)
+        #normalized_returns = (returns - np.mean(returns)) / (np.std(returns) + 1e-10)
+        normalized_returns = returns - np.mean(returns)
+        normalized_returns = torch.tensor(normalized_returns).to(Parameters.device)
         log_probabilities = torch.stack(self.policy.log_probablities).flatten().to(Parameters.device)
 
-        loss = -log_probabilities * returns
+        loss = -log_probabilities * normalized_returns
         # Loss needs to be an item, not an tensor.
-        loss = loss[0]
+        loss = torch.sum(loss)
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -118,7 +119,7 @@ class Reinforce_Agent():
         self.game.takeAction(action)
 
         reward = Utility.get_reward_from_dictionary(self.game.getMergedCellsAfterMove())
-
+        # Small preprocessing
         # Store min and max reward for statistics
         data_helper.store_min_max_reward(reward)
 

Environment/Reinforce_Policy.py
@@ -8,7 +8,7 @@ class Reinforce_Policy(nn.Module):
     def __init__(self, history):
         super(Reinforce_Policy, self).__init__()
 
-        self.model = nn.Sequential(*Parameters.layers) 
+        self.model = nn.Sequential(*(Parameters.layers.copy())).to(Parameters.device()) 
 
         self.history = history
 

Environment/Utility.py
@@ -4,9 +4,9 @@ import pandas as pd
 import os
 import glob
 from datetime import datetime 
-from Enums import RewardFunctions
+from Enums import RewardFunctions, Optimizers
 from Parameters import Parameters
-
+import torch.optim as optim
 
 class Utility(ABC):
 
@@ -175,13 +175,29 @@ class Utility(ABC):
         time_of_experiment = datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
         return time_of_experiment
 
+    @staticmethod
     def get_parameters_class_as_json():
         json_content = {}
-        json_content['Episodes']      = Parameters.episodes
-        json_content['Learning rate'] = Parameters.lr
-        json_content['Gamma']         = Parameters.gamma
-        json_content['Board size']    = Parameters.board_size
-        json_content['Input size']    = Parameters.input_size
-        json_content['Output size']   = Parameters.output_size
-        json_content['Model']         = repr(Parameters.layers)
+        json_content['Episodes']        = Parameters.episodes
+        json_content['Learning rate']   = Parameters.lr
+        json_content['Momentum']        = Parameters.momentum
+        json_content['Gamma']           = Parameters.gamma
+        json_content['Board size']      = Parameters.board_size
+        json_content['Input size']      = Parameters.input_size
+        json_content['Output size']     = Parameters.output_size
+        json_content['Optimizer']       = str(Optimizers(Parameters.optimizer).name)
+        json_content['Reward function'] = str(RewardFunctions(Parameters.reward_type).name)
+        json_content['Model']           = repr(Parameters.layers)
         return json_content
+
+    @staticmethod
+    def get_optimizer_for_parameters(parameters):
+        optimizer = None
+        if Parameters.optimizer == Optimizers.ADAM:
+            optimizer = optim.Adam(parameters, lr = Parameters.lr)
+        elif Parameters.optimizer == Optimizers.RMSPROP:
+            optimizer = optim.RMSprop(parameters, lr = Parameters.lr, momentum = Parameters.momentum)
+        elif Parameters.optimizer == Optimizers.SGD:
+            optimizer = optim.SGD(parameters, momentum = Parameters.momentum)
+
+        return optimizer